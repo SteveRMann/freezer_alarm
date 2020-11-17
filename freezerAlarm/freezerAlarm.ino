f8ff47a1995dca4fbeb0e07e303d798106515523
Stephen Mann <steve@mann.ws>
2020-11-16 23:49:41 -0500

    Add files via upload

---
+++ freezerAlarm/freezerAlarm.ino
@@ -0,0 +1,193 @@
+#define SKETCH_NAME "freezerAlarm.ino"
+#define SKETCH_VERSION "2.0"
+
+
+/*
+     Board: Lolin(Wemos) D1 R2 & Mini
+
+     This project is for a freezer alarm.
+     A DS18B20 will be inside the freezer and the Wemos D1 Mini with the OLED shield will be outside.
+     Temperatures are also published on MQTT:
+       fahrenheightTopic= freezer/temp/f
+       centigradeTopic= freezer/temp/c
+
+*/
+
+
+#include <OneWire.h>            // Driver for DS18S20, DS18B20, DS1822 Temperature Sensor.
+#include "SSD1306Wire.h"        // Wire.h includes OLEDDisplay.h which contains the command definitions.
+
+#define HOSTPREFIX "FRZR"
+#include "ESP8266WiFi.h"        // Not needed if also using the Arduino OTA Library...
+#include "D:\River Documents\Arduino\libraries\Kaywinnet.h"  \\ WiFi credentials
+#include <PubSubClient.h>       // connect to a MQTT broker and publish/subscribe messages in topics.
+
+// setup_wifi vars
+char macBuffer[24];             // Holds the last three digits of the MAC, in hex.
+char hostNamePrefix[] = HOSTPREFIX;
+char hostName[24];              // Holds hostNamePrefix + the last three bytes of the MAC address.
+
+
+// Declare an object of class WiFiClient, which allows to establish a connection to a specific IP and port
+// Declare an object of class PubSubClient, which receives as input of the constructor the previously defined WiFiClient.
+// The constructor MUST be unique on the network.
+WiFiClient frzrClient;
+PubSubClient client(frzrClient);
+
+#define NODENAME "freezer"                             // Give this node a name
+const char *cmndTopic = NODENAME "/cmnd";              // Incoming commands, payload is a command.
+const char *statusTopic = NODENAME "/status";
+const char *fahrenheightTopic = NODENAME "/temp/f";
+const char *centigradeTopic = NODENAME "/temp/c";
+const char *connectName =  NODENAME "1";               // Must be unique on the network
+const char *mqttServer = mqtt_server;                  // Local broker defined in Kaywinnet.h
+const int mqttPort = 1883;
+
+
+// Ceate instances of OneWire and SSD1360Wire
+OneWire  ds(D4);                                      // ds18b20 on pin D4
+SSD1306Wire  display(0x3c, D2, D1);                   // I2C address and pins for Wemos D1 Mini (0x3c,sda,scl, geometry)
+//                                                    // Geometry =0 for 128x64 (default), =1 for 128x32
+
+
+#define DEBUG true  //set to true for debug output, false for no debug ouput
+#define Serial if(DEBUG)Serial
+
+
+
+// =================================== setup() ===================================
+void setup(void) {
+  beginSerial();
+  setup_wifi();
+
+  // Call the setServer method on the PubSubClient object, passing as first argument the
+  // address and as second the port.
+  client.setServer(mqttServer, mqttPort);
+  mqttConnect();
+
+  //Show the topics:
+  Serial.print(F("statusTopic= "));
+  Serial.println(statusTopic);
+  Serial.print(F("fahrenheightTopic= "));
+  Serial.println(fahrenheightTopic);
+  Serial.print(F("centigradeTopic= "));
+  Serial.println(centigradeTopic);
+  Serial.println();
+
+
+
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setBrightness(16);
+
+  display.clear();                                      // clear the display
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  //display.drawString(0, 0, "ds18b20-mqtt.ino");
+  display.drawString(0, 0, SKETCH_NAME);
+  display.display();
+  delay(3000);
+
+}
+
+
+// ==================================== loop() ====================================
+void loop(void) {
+  //Make sure we stay connected to the mqtt broker
+  if (!client.connected()) {
+    mqttConnect();
+  }
+  if (!client.loop()) {
+    client.connect(connectName);
+  }
+
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+
+  if (!ds.search(addr)) {
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+
+
+  if (OneWire::crc8(addr, 7) != addr[7])  {
+    Serial.println("CRC is not valid!");
+    return;
+  }
+
+  // the first ROM byte indicates which chip
+  switch (addr[0])
+  {
+    case 0x10:
+      type_s = 1;
+      break;
+    case 0x28:
+      type_s = 0;
+      break;
+    case 0x22:
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);                // Start conversion
+  delay(1000);
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);
+
+  // Read Scratchpad
+  for ( i = 0; i < 9; i++) {
+    data[i] = ds.read();
+  }
+
+  // Convert the data to actual temperature
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3;                // 9 bit resolution default
+    if (data[7] == 0x10)
+    {
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    if (cfg == 0x00) raw = raw & ~7;                    // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3;               // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1;               // 11 bit res, 375 ms
+  }
+
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print(F("  Temperature = "));
+  Serial.print(celsius);
+  Serial.print(F(" Celsius, "));
+  Serial.print(fahrenheit);
+  Serial.println(F(" Fahrenheit"));
+  Serial.println();
+
+  // Send the temperature to the OLED display
+  display.clear();                                      // clear the display
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);                    // font can be 10, 16 or 24
+  display.drawString(0, 0, "Temperature");
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 16, String(fahrenheit) + " °F");
+  display.drawString(0, 40, String(celsius) + " °C");
+  display.display();
+
+  // Publish the temperature on MQTT.
+  // dtostrf(floatvar, StringLengthIncDecimalPoint, numVarsAfterDecimal, charbuf);
+  char result[6];
+  dtostrf(celsius, 4, 2, result);
+  client.publish(centigradeTopic, result);
+}
---
+++ freezerAlarm/wifi.ino
@@ -0,0 +1,109 @@
+// setup_wifi
+// ============================= Connect the ESP to the router =============================
+// Connect to WiFi network so we can reach the MQTT broker and publish messages to topics.
+
+/*
+  Make sure you include at the start of the sketch:
+  #define HOSTPREFIX "CGM-"
+  #include "ESP8266WiFi.h"   // Not needed if also using the Arduino OTA Library...
+  #include "D:\River Documents\Arduino\libraries\Kaywinnet.h"  \\ WiFi credentials
+
+  If using the OTA Library, put these at the start of the sketch.
+  // setup_wifi vars
+  char macBuffer[24];       // Holds the last three digits of the MAC, in hex.
+  char hostNamePrefix[] = HOSTPREFIX;
+  char hostName[24];        // Holds hostNamePrefix + the last three bytes of the MAC address.
+*/
+
+void setup_wifi() {
+#ifndef Kaywinnet
+#include "D:\River Documents\Arduino\libraries\kaywinnet.h"
+#endif
+  byte mac[6];                     //// the MAC address of your Wifi shield
+
+  Serial.println(F("\n"));
+  Serial.print(F("Connecting to "));
+  Serial.println(my_ssid);
+
+
+  WiFi.mode(WIFI_STA);
+  WiFi.enableInsecureWEP();
+  WiFi.begin(my_ssid, my_password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(WiFi.status()); Serial.print(F(" "));
+  }
+  Serial.println(F("\nWiFi connected, "));
+  Serial.print(F("MAC Address: "));
+  Serial.println(WiFi.macAddress());
+  Serial.print(F("IP address: "));
+  Serial.println(WiFi.localIP());
+
+
+  // Get the last three numbers of the mac address.
+  // "4C:11:AE:0D:83:86" becomes "0D8386" in macBuffer.
+  WiFi.macAddress(mac);
+  snprintf(macBuffer, sizeof(macBuffer), "%02X%02X%02X", mac[3], mac[4], mac[5]);
+
+  // Build hostName from hostNamePrefix + last three bytes of the MAC address.
+  strcpy(hostName, hostNamePrefix);
+  strcat(hostName, "-");
+  strcat(hostName, macBuffer);
+
+  Serial.print(F("hostName = \""));
+  Serial.print(hostName);
+  Serial.println(F("\""));
+
+}
+
+
+
+//=============================== macToStr ===============================
+/* Make a string from the MAC address
+  Useage:
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  Serial.print(F("MAC Address: "));
+  Serial.println(macToStr(mac));
+*/
+
+String macToStr(const uint8_t* mac) {
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    if (mac[i] < 10) {
+      result += '0';
+      result += String(mac[i], 16);
+    }
+    else {
+      result += String(mac[i], 16);
+    }
+    if (i < 5)  // Add a colon between hex duplets
+      result += ':';
+  }
+  return result;
+}
+
+
+/*
+  //============================= Reconnect to the MQTT Broker =============================
+  void reconnect()
+  {
+  while (!client.connected()) {
+    Serial.println(F("\nAttempting MQTT connection..."));
+
+    if (client.connect(hostName))  {            //hostName is built in setup_wifi()
+      Serial.print(F("connected, "));
+  //      Serial.println(F("Subscribed to:"));
+  //      Serial.println(cmdTopic);
+
+  //      client.subscribe(unlockTopic);
+    }
+    else {
+      Serial.print(F("failed, rc= "));
+      Serial.print(client.state());
+      Serial.println(F(", trying again in 5 seconds."));
+      delay(5000);
+    }
+  }
+  }
+*/
